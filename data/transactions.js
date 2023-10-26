48c4faaf62a0568566df256df4215254c1851a9c
Maxi <mperchik@protonmail.com>
2023-10-26 20:07:22 -0300

    + endpoint prueba que transactions funciona

---
+++ data/transactions.js
@@ -0,0 +1,19 @@
+const { ObjectId } = require("mongodb");
+const conn = require("./conn");
+const DATABASE = "sample_analytics";
+const TRANSACTIONS = "transactions";
+
+async function getAllTransactions(pageSize, page) {
+  const connectiondb = await conn.getConnection();
+  const transactions = await connectiondb
+    .db(DATABASE)
+    .collection(TRANSACTIONS)
+    .find({})
+    .limit(pageSize)
+    .skip(pageSize * page)
+    .toArray();
+
+  return transactions;
+}
+
+module.exports = { getAllTransactions}
